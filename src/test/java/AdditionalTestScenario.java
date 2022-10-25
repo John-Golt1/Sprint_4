010bd9028716bd4cf969313b862e62acea97f0a5
Anton Makurin <lev.merk14@gmail.com>
2022-10-25 22:00:32 +0300

    I can't push...

--- src/test/java/AdditionalTestScenario.java
+++ src/test/java/AdditionalTestScenario.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class AdditionalTestScenario {
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertTrue;
+import java.time.Duration;
+public class AdditionalTestScenario {
+    WebDriver driver = new ChromeDriver();
+    //WebDriver driver = new FirefoxDriver();
+    @Test
+    public void examWorkButtonScooter() {  //проверяем, что после нажатия на кнопку Самокат - попадаем на стартовую страницу
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        MainSamokat object = new MainSamokat(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        object.scooterButton();
+        assertTrue("Ops", object.scooterImgTrue());
+    }
+    @Test
+    public void examStatusOrder() { //проверяем, что при неправильном номере заказа, появляется картинка "такого заказа нет"
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        MainSamokat object = new MainSamokat(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        object.orderButton();
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.
+                visibilityOfElementLocated(By.cssSelector("[alt='Not found']")));
+        assertTrue("Img true", object.noFindImg());
+    }
+    @After
+    public void closeTest(){
+        driver.quit();
+    }
 }
